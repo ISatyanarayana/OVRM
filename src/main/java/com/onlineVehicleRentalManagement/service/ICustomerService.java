1257e9c2c1287198580a364e98216a6f4294addd
G SUDHAKAR REDDY <145975697+G-SUDHAKAR-REDDY@users.noreply.github.com>
2023-11-02 18:14:16 +0530

    Implemented Customer module. (#4)

--- src/main/java/com/onlineVehicleRentalManagement/service/ICustomerService.java
+++ src/main/java/com/onlineVehicleRentalManagement/service/ICustomerService.java
@@ -3,13 +3,14 @@ package com.onlineVehicleRentalManagement.service;
 import java.util.List;
 
 import com.onlineVehicleRentalManagement.entity.Customer;
-
+import com.onlineVehicleRentalManagement.exceptions.CustomerAlreadyExistsException;
+import com.onlineVehicleRentalManagement.exceptions.ResourceNotFoundException;
 public interface ICustomerService {
 
-	public Customer addCustomer(Customer customer);
-	public Customer removeCustomer(Customer customer);
-	public Customer updateCustomer(Customer customer);
-	public Customer viewCustomer(Customer customer);
+	public Customer addCustomer(Customer customer) throws CustomerAlreadyExistsException ;
+	public Customer removeCustomer(Long customerId) throws ResourceNotFoundException;
+	public Customer updateCustomer(Customer customer) throws ResourceNotFoundException;
+	public Customer viewCustomer(Long customerId) throws ResourceNotFoundException;
 	public List<Customer> viewAllCustomer(String vtype);
 	public List<Customer> viewAllCustomersByLocation(Customer customer);
 }

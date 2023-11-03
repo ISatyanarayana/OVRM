9880b316f734c6752cc731096d29999fcc5b6030
ISatyanarayana <rajuindukuri.2001@gmail.com>
2023-11-03 12:25:33 +0530

    implemented booking controller

--- src/main/java/com/onlineVehicleRentalManagement/repository/IBookingRepository.java
+++ src/main/java/com/onlineVehicleRentalManagement/repository/IBookingRepository.java
@@ -1,8 +1,10 @@
 package com.onlineVehicleRentalManagement.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import com.google.common.base.Optional;
 import com.onlineVehicleRentalManagement.entity.Booking;
 
 @Repository
--- src/main/java/com/onlineVehicleRentalManagement/service/IBookingService.java
+++ src/main/java/com/onlineVehicleRentalManagement/service/IBookingService.java
@@ -15,7 +15,7 @@ public interface IBookingService {
 	public Booking addBooking(Booking b,LocalDate fromDate,LocalDate toDate) throws BookingAlreadyExistsException;
 	public Booking updateBooking(Booking b) throws ResourceNotFoundException;
 	public Booking cancelBooking(Booking b) throws ResourceNotFoundException;
-	public Booking viewBooking(Booking b) throws ResourceNotFoundException;
+	public Booking viewBooking(long bookingId) throws ResourceNotFoundException;
 	public List<Booking> viewAllBookings(Vehicle v) throws ResourceNotFoundException;
 	public List<Booking> viewAllBookings(Customer customer) throws ResourceNotFoundException;
 	public List<Booking> viewAllBookingByDate(LocalDate bdate) throws ParseException, ResourceNotFoundException;
